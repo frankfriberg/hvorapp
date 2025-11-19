ff53869b17675663dd188396a26d7f63bf7a103f
Frank Friberg <frankfriberg@users.noreply.github.com>
2025-11-19 15:43:34 +0100

    chore: remove blob domain reference

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    domains: ["wiqjiwueociarhfj.public.blob.vercel-storage.com"],
-  },
   output: "standalone",
 };
 
